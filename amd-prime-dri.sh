01cf56123cfb849afc3a602d9c8e5f35cc919b38
Matt Hartley <matt.hartley@gmail.com>
2023-12-28 19:19:34 -0800

    Create amd-prime-dri.sh
    First publishing of amd-prime-dri.sh - a simple script to detect if the discrete GPU is in use.

---
+++ amd-prime-dri.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# List all processes with their PIDs and command names
+ps -e -o pid=,comm= | while read -r proc_pid proc_name
+do
+    # Check if the environment file exists for the process
+    if [ -f /proc/$proc_pid/environ ]; then
+        # Attempt to get DRI_PRIME setting for the process
+        gpu_usage=$(grep -az "DRI_PRIME" /proc/$proc_pid/environ)
+
+        # If DRI_PRIME is set, display the process information
+        if [ ! -z "$gpu_usage" ]; then
+            # Display process name, PID, and GPU usage info
+            echo "Process '${proc_name}' (ID ${proc_pid}) is using GPU: ${gpu_usage}"
+        fi
+    fi
+done
